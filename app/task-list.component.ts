06644befb217007af1e8e9e1915cdd3250a027d3
Kevin VanEvery <kvanever@gmail.com>
2016-08-09 11:59:49 -0700

    add new task form

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -2,12 +2,13 @@ import { Component, EventEmitter } from 'angular2/core';
 import { TaskComponent } from './task.component';
 import { Task } from './task.model';
 import { EditTaskDetailsComponent } from './edit-task-details.component';
+import { CreateNewTaskComponent } from './create-new-task.component';
 
 @Component({
   selector: 'task-list',
   inputs: ['taskList'],
   outputs: ['onTaskSelect'],
-  directives: [TaskComponent, EditTaskDetailsComponent],
+  directives: [TaskComponent, EditTaskDetailsComponent, CreateNewTaskComponent],
   templateUrl: 'app/task-list.view.html'
 })
 export class TaskListComponent {
@@ -22,4 +23,9 @@ export class TaskListComponent {
     this.selectedTask = clickedTask;
     this.onTaskSelect.emit(clickedTask);
   }
+  createTask(description: string): void {
+  this.taskList.push(
+    new Task(description, this.taskList.length)
+  );
+}
 }
