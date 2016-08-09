6afee0e1d0843e2c7ae696a82384e58308a3db92
Kevin VanEvery <kvanever@gmail.com>
2016-08-09 11:35:27 -0700

    allow dynamic editing of to do list description

--- app/edit-task-details.component.ts
+++ app/edit-task-details.component.ts
@@ -5,7 +5,10 @@ import { Task } from './task.model';
   selector: 'edit-task-details',
   inputs: ['task'],
   template: `
-    <h3>Edit Description: {{ task.description }}</h3>
+    <div class="task-form">
+      <h3>Edit Description:</h3>
+      <input [(ngModel)]="task.description" class="col-sm-8 input-lg task-form"/>
+    </div>
   `
 })
 export class EditTaskDetailsComponent {
